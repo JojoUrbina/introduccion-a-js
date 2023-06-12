4150931fb0617f2bf2aa7b1a5418050d5741aa40
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-12 16:42:09 -0300

    Se completo tarea 2

--- tareas/clase-1/tarea-clase-1.js
+++ tareas/clase-1/tarea-clase-1.js
@@ -12,80 +12,47 @@
 // let edadActual=calcularEdad(anioActual,anioNacimiento)
 // console.log(`tu edad actual es ${edadActual}`)
 
-
 // Preguntar el salario anual y calcular el salario mensual
 // Preguntar el salario mensual y calcular el anual
 // diario... semanal, por hora. etc.
-let salarioClienteAnual=Number()
-let salarioClienteMensual=Number()
-const mesesPorAnio=12;
+let salarioClienteAnual = Number(prompt("indica salario anual"));
+let salarioClienteMensual = Number(prompt("indica salario mensual"));
+const DIAS_POR_MES=30;
+const DIAS_POR_SEMANA=7;
+const mesesTrabajoPorAnio = 12;//Se prodria preguntar al cliente
+const diasTrabajoPorSemana = 5;//pero para no hacerlo tan largo
+const diasTrabajoPorMes = Math.round(diasTrabajoPorSemana * (DIAS_POR_MES / DIAS_POR_SEMANA));
+const horasTrabajoPorDia = 8;
+
+function calcularSalarioAnual(salarioClienteMensual) {
+    return salarioClienteMensual * mesesTrabajoPorAnio;
+}
 
 function calcularSalarioMensual(salarioClienteAnual) {
-    return salarioClienteAnual/mesesPorAnio
-}
-function calcularSalarioAnual(salarioClienteMensual) {
-    return salarioClienteMensual*mesesPorAnio
+  return salarioClienteAnual / mesesTrabajoPorAnio;
 }
 
-
-
-
-/// SCOPE
-
-// Variable hoisting -> izar
-// console.log(hola); //Falla porque no está definida
-
-// console.log(mensaje); //No falla, pero muestra undefined... por qué?
-// var mensaje = 'Hola, mundo';
-// console.log(mensaje); //Hola, mundo
-
-// Y con let?
-// let mensaje = 'Hola, mundo';
-// console.log(mensaje); //error
-
-// function hoisting
-
-/*
-pruebaHoisting(); //funciona!
-function pruebaHoisting(){
-    console.log('prueba');
+function calcularSalarioSemana(salarioClienteMensual) {
+  return salarioClienteMensual / diasTrabajoPorSemana;
 }
-//pruebaHoisting();
-*/
-
-/*
-var a = 1; //global
-let b = 2; //global
-
-function prueba(c) { //c es un parámetro de la función prueba. LOCAL.
-    let d = 4; //local a la función
-
-    if(c === 3){
-        var e = 5; // por hoisting termina siendo local a la función
-        let f = 6; // local al IF
-
-        console.log('a dentro del if vale: ' + a); // 1
-        console.log('b dentro del if vale: ' + b); // 2
-        console.log('c dentro del if vale: ' + c); // lo que sea que le pasen a prueba
-        console.log('d dentro del if vale: ' + d); // 4
-        console.log('e dentro del if vale: ' + e); // 5
-        console.log('f dentro del if vale: ' + f); // 6
-    }
-
-    console.log('a dentro de la funcion pero fuera del if, vale: ' + a); // 1
-    console.log('b dentro de la funcion pero fuera del if, vale: ' + b); // 2
-    console.log('c dentro de la funcion pero fuera del if, vale: ' + c); // lo que sea que le pasen a prueba
-    console.log('d dentro de la funcion pero fuera del if, vale: ' + d); // 4
-    console.log('e dentro de la funcion pero fuera del if, vale: ' + e); // 5
-    // console.log('f dentro de la funcion pero fuera del if, vale: ' + f); // error
+function calcularSalarioDiario(salarioClienteMensual) {
+  return salarioClienteMensual / diasTrabajoPorMes;
+}
+function calcularSalarioHoras(salarioClienteMensual) {
+  return calcularSalarioDiario(salarioClienteMensual) / horasTrabajoPorDia;
 }
 
-prueba(3);
 
-console.log('a vale: ' + a); // 1
-console.log('b vale: ' + b); // 2
-// console.log('c vale: ' + c); // error
-// console.log('d vale: ' + d); // error
-// console.log('e vale: ' + e); // error
-// console.log('f vale: ' + f); // error
-*/
+console.log(`
+el salario mensual es : 
+${calcularSalarioMensual(salarioClienteAnual)}
+el anual es :
+${calcularSalarioAnual(salarioClienteMensual)}
+el semanal es :
+${calcularSalarioSemana(salarioClienteMensual)}
+el diario es :
+${calcularSalarioDiario(salarioClienteMensual)}
+por hora es:
+${calcularSalarioHoras(salarioClienteMensual)}
+`
+);
