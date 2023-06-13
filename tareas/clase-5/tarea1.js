17e17a3f2ba2e3e18044a1de77868fce1cc86834
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-13 20:52:28 -0300

    complete tarea 1 de la clase 6

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -1,12 +1,10 @@
-document.querySelector('#calcular-salario-mensual').onclick = function(){
-    const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual);
-
-    document.querySelector('#salario-mensual').value = salarioMensual;
-
-    return false;
-}
-
-function calcularSalarioMensual(salarioAnual){
+function calcularSalarioMensual(salarioAnual) {
     return salarioAnual / 12;
-}
+  }
+  
+  document.querySelector("#calcular").onclick = (e) => {
+    e.preventDefault;
+    let $salarioAnual = document.querySelector("#salario-anual").value;
+    document.querySelector("#salario-mensual").value =
+      calcularSalarioMensual($salarioAnual);
+  };
