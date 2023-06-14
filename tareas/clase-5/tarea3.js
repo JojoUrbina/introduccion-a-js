679e2fcfe4903dd60bc903c4f6fc64c6b571d0d6
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-13 23:49:40 -0300

    tarea 3 de la clase 6 terminada

--- tareas/clase-5/tarea3.js
+++ tareas/clase-5/tarea3.js
@@ -8,6 +8,7 @@
 
 let $formulario = document.querySelector("form");
 let $botonAgregarInput = document.querySelector("#agregarInput");
+let $botonCalcular = document.querySelector("#calcular");
 function crearInput(cantidadClases) {
   for (let i = 0; i < cantidadClases; i++) {
     let $div = document.createElement("div");
@@ -33,10 +34,46 @@ function eliminarDiv() {
     divsExistentes[i].remove();
   }
 }
+function sumarHoras() {
+  let total = 0;
+  let arrayHoras = document.querySelectorAll(".inputHora");
+  for (let i = 0; i < arrayHoras.length; i++) {
+    total += Number(arrayHoras[i].value);
+  }
+  return total;
+}
+function sumarMinutos() {
+  let total = 0;
+  let arrayMinutos = document.querySelectorAll(".inputMinutos");
+  for (let i = 0; i < arrayMinutos.length; i++) {
+    total += Number(arrayMinutos[i].value);
+  }
+  return total;
+}
+function sumarSegundos() {
+  let total = 0;
+  let arraySegundos = document.querySelectorAll(".inputSegundos");
+  for (let i = 0; i < arraySegundos.length; i++) {
+    total += Number(arraySegundos[i].value);
+  }
+  return total;
+}
 
 $botonAgregarInput.onclick = function (e) {
   e.preventDefault;
   let $cantidadClases = document.querySelector("#clases").value;
   eliminarDiv();
   crearInput($cantidadClases);
+  console.log(document.querySelectorAll(".inputHora"));
+};
+$botonCalcular.onclick = function (e) {
+  e.preventDefault;
+  let segundos = sumarSegundos();
+  let minutos = sumarMinutos();
+  let horas = sumarHoras();
+  minutos += Math.floor(segundos / 60);
+  let totalHoras = horas + Math.floor(minutos / 60);
+  let minutosRestantes= minutos%60
+  let segundosRestantes= segundos%60
+  console.log(totalHoras+" "+minutosRestantes+" "+segundosRestantes)
 };
