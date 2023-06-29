3b76b57f3d37148149e41e350a2ec5e5aad258ac
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-29 17:51:33 -0300

    Se agrego boton para empezar de nuevo en el index, se corrigieron las funciones de calculo y se mejoro la funcion empezar de nuevo

--- tareas/clase-6/tarea 6/tarea1.js
+++ tareas/clase-6/tarea 6/tarea1.js
@@ -8,125 +8,97 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 let $formulario = document.querySelector("form");
 let $botonAgregarFamiliares = document.querySelector("#agregar-familiares");
 let $botonCalcular = document.querySelector("#calcular");
-
-let $resultado = document.querySelector("#resultado");
-//let $botonEmpezarDeNuevo = document.createElement("button");
-//$botonEmpezarDeNuevo.textContent = "Empezar de nuevo";
-//$botonEmpezarDeNuevo.type = "button";
-//let arrayEdadesFamiliares = [];
-
 $botonAgregarFamiliares.onclick = function (e) {
-  //EmpezarDeNuevo();
-  
   let cantidadFamiliares = document.querySelector("#cantidad-familiares").value;
 
   crearFamiliares(cantidadFamiliares);
   e.preventDefault();
 };
 
-
 function crearFamiliares(cantidadFamiliares) {
-let $divFamiliar=document.querySelector("#familiares")
-  for (let i = 1; i <= cantidadFamiliares; i++) {
-    
-    let $div = document.createElement("div");
-    $div.className = "divFamiliar";
+  EmpezarDeNuevo();
 
-    let $label= document.createElement("label");
+  let $divFamiliar = document.querySelector("#familiares");
+  for (let i = 1; i <= cantidadFamiliares; i++) {
+    let $div = document.createElement("div");
+    $div.className = "div-familiar";
+
+    let $label = document.createElement("label");
     $label.innerText = `Familiar #${i}: `;
-    
-    let $input= document.createElement("input");
+
+    let $input = document.createElement("input");
     $input.placeholder = "ingresar Edad";
-    $input.className="edad-familiar"
-   
+    $input.className = "edad-familiar";
+
     $div.appendChild($label);
     $div.appendChild($input);
     $divFamiliar.appendChild($div);
   }
-
- // $formulario.appendChild($botonEmpezarDeNuevo);
 }
+function EmpezarDeNuevo() {
+  document.querySelector("#resultado").innerText = "";
+  let divsExistentes = document.querySelectorAll(".div-familiar");
+  for (let i = 0; i < divsExistentes.length; i++) {
+    divsExistentes[i].remove();
+  }
+}
+
 function extraerEdades() {
-let $edadesFamiliares=document.querySelectorAll(".edad-familiar")
-let edadesExtraidas=[];
+  let $edadesFamiliares = document.querySelectorAll(".edad-familiar");
+  let edadesExtraidas = [];
   for (let i = 0; i < $edadesFamiliares.length; i++) {
     edadesExtraidas.push(Number($edadesFamiliares[i].value));
   }
   return edadesExtraidas;
 }
 
+function calcularFamiliarMenor(edadesFamiliares) {
+  let familiarMenor = edadesFamiliares[0];
+  for (let i = 1; i < edadesFamiliares.length; i++) {
+    if (edadesFamiliares[i] < familiarMenor) {
+      familiarMenor = edadesFamiliares[i];
+    }
+  }
+  return familiarMenor;
+}
 
+function calcularFamiliarMayor(edadesFamiliares) {
+  let familiarMayor = edadesFamiliares[0];
+  for (let i = 1; i < edadesFamiliares.length; i++) {
+    if (edadesFamiliares[i] > familiarMayor) {
+      familiarMayor = edadesFamiliares[i];
+    }
+  }
+  return familiarMayor;
+}
+function calcularPromedioEdadFamiliares(edadesFamiliares) {
+  let total = 0;
+  for (let i = 0; i < edadesFamiliares.length; i++) {
+    total += edadesFamiliares[i];
+  }
+  return total / edadesFamiliares.length;
+}
 
+$botonCalcular.onclick = function (e) {
+  let edadesFamiliares = extraerEdades();
 
+  let familiarMayor = calcularFamiliarMayor(edadesFamiliares);
+  let familiarMenor = calcularFamiliarMenor(edadesFamiliares);
+  let promedioEdadFamilia = calcularPromedioEdadFamiliares(edadesFamiliares);
+  console.log(familiarMayor);
+  console.log(familiarMenor);
+  console.log(promedioEdadFamilia);
 
+  let $resultado = document.querySelector("#resultado");
+  $resultado.innerText = `El familiar con mas edad tiene ${familiarMayor} años, el menor tiene ${familiarMenor} ${
+    familiarMenor > 1 ? "años " : "año "
+  }y  el promedio de edad en la familia es de ${promedioEdadFamilia} años`;
+  e.preventDefault();
+};
 
-
-
-
-
-
-
-// function calcularFamiliarMenor(arrayEdadesFamiliares) {
-//   familiarMenor = arrayEdadesFamiliares[0];
-//   for (let i = 1; i < arrayEdadesFamiliares.length; i++) {
-//     if (arrayEdadesFamiliares[i] < familiarMenor) {
-//       familiarMenor = arrayEdadesFamiliares[i];
-//     }
-//   }
-//   return familiarMenor;
-// }
-// function calcularFamiliarMayor(arrayEdadesFamiliares) {
-//   familiarMayor = arrayEdadesFamiliares[0];
-//   for (let i = 1; i < arrayEdadesFamiliares.length; i++) {
-//     if (arrayEdadesFamiliares[i] > familiarMayor) {
-//       familiarMayor = arrayEdadesFamiliares[i];
-//     }
-//   }
-//   return familiarMayor;
-// }
-// function calcularPromedioEdadFamiliares(arrayEdadesFamiliares) {
-//   promedioEdadFamiliares = Number(
-//     arrayEdadesFamiliares.reduce((acc, act) => acc + act, 0) /
-//       arrayEdadesFamiliares.length
-//   );
-//   return promedioEdadFamiliares;
-// }
-
-
-
-
-
-// $botonCalcular.onclick = function (e) {
- 
-//   let $edadesFamiliares = document.querySelectorAll(".inputEdadesFamiliares");
-//   arrayEdadesFamiliares = extraerEdades($edadesFamiliares);
-//   calcularFamiliarMayor(arrayEdadesFamiliares);
-//   calcularFamiliarMenor(arrayEdadesFamiliares);
-//   calcularPromedioEdadFamiliares(arrayEdadesFamiliares);
-
-//   $resultado.innerText = `El familiar con mas edad tiene ${familiarMayor} años, el menor tiene ${familiarMenor} ${
-//     familiarMenor > 1 ? "años " : "año "
-//   }y  el promedio de edad en la familia es de ${promedioEdadFamiliares} años`;
-//   e.preventDefault();
-
-// };
-
-
-// $botonEmpezarDeNuevo.onclick = function (e) {
-//   e.preventDefault;
-//   EmpezarDeNuevo();
-//   document.querySelector("#cantidadFamiliares").value = "";
-//   arrayEdadesFamiliares = [];
-// };
-
-// function EmpezarDeNuevo(e) {
-//   $resultado.innerText = "";
-//   familiarMenor = 0;
-//   familiarMayor = 0;
-//   promedioEdadFamiliares = 0;
-//   $botonEmpezarDeNuevo.remove();
-//   let divsExistentes = document.querySelectorAll(".divInput");
-//   for (let i = 0; i < divsExistentes.length; i++) {
-//     divsExistentes[i].remove();
-//   }
-// }
+ $botonEmpezarDeNuevo.onclick = function (e) {
+   e.preventDefault;
+   EmpezarDeNuevo();
+   document.querySelector("#cantidadFamiliares").value = "";
+   arrayEdadesFamiliares = [];
+ };
